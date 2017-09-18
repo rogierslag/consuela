21c8f20ec349cec2d55232ab14119d7fbde87ee5
Alex Kolpa <akolpa1990@gmail.com>
2017-09-18 23:29:09 +0200

    Add merge locking support
    This adds two new endpoints for setting and releasing merge locks on pull requests
    Additionally, using a global variable the lock state is maintained on changes to pull requests

--- README.md
+++ README.md
@@ -8,6 +8,17 @@ Consuela is a tool we use at [inventid](https://www.inventid.nl) to prevent acci
 It works by checking tags of your Pull request.
 In case a PR has one of the mentioned tags, Consuela will drop a status to your PR to prevent merges.
 
+
+## Merge locking
+
+Additionally, Consuela offers support for locking repositories entirely through the `/merge-lock` endpoint.
+This puts an error status on all the open pull request, indicating an ongoing merge lock
+The endpoints can be accessed using `POST` for setting the lock, and `DELETE` for releasing it.
+Both endpoints require a `repo` query parameter to be set, and the lock creation optionally supports a `message` query param for indicating why the lock is in place.
+
+One caveat of the current locking system is that it uses a global variable to store the current lock state.
+That means that if a lock is in place and Consuela is restarted, any changes to pull requests will not be updated with the lock status.
+
 # What does it look like?
 
 ![screenshot 2015-05-08 21 11 18](https://cloud.githubusercontent.com/assets/2778689/7543879/740e1df2-f5c8-11e4-95a5-9dd3032efda2.png)
--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ import express from 'express';
 import config from 'config';
 import bodyParser from 'body-parser';
 import request from 'request';
+import parseLinkHeader from 'parse-link-header';
 
 import log from './log';
 import promisify from './promisify';
@@ -13,9 +14,14 @@ const app = express();
 app.use(bodyParser.json());
 app.get('/health', checkHealth);
 app.post('/', checkPayload);
+app.post('/merge-lock', validateRepo, putMergeLock);
+app.delete('/merge-lock', validateRepo, releaseMergeLock);
 
 const supportedActions = ['opened', 'labeled', 'unlabeled', 'synchronize'];
 const port = 8543;
+
+//global merge lock (so we update the check if people push new commits)
+let mergeLock = null;
 
 // And listen!
 const server = app.listen(port, () => {
@@ -37,6 +43,16 @@ function checkHealth(req, res) {
 	log.info('Processed successful healthcheck');
 }
 
+function validateRepo(req, res, next) {
+	if (config.get('repos').includes(req.query.repo.toLowerCase())) {
+		next();
+	}
+	else {
+		log.warn(`Repo ${req.query.repo} is not allowed`);
+		res.sendStatus(403);
+	}
+}
+
 // Determine whether to do something with the payload
 async function checkPayload(req, res) {
 	if (req.body.zen) {
@@ -55,13 +71,25 @@ async function checkPayload(req, res) {
 		log.info(`Received a request for matching repo: ${req.body.pull_request.head.repo.full_name}`);
 		log.info(`Action is: ${req.body.action}`);
 		if (supportedActions.includes(req.body.action)) {
+			const pullRequest = req.body.pull_request;
 			try {
-				await checkLabels(req.body.pull_request);
+				await checkLabels(pullRequest);
 				res.sendStatus(200);
 			}
 			catch (e) {
 				log.info(`Error while checking labels: ${e}`);
 				res.sendStatus(500);
+			}
+
+			if(mergeLock) {
+				try {
+					await setStatus(pullRequest.head.repo.full_name, pullRequest.head.sha, mergeLock);
+					res.sendStatus(200);
+				}
+				catch (e) {
+					log.info(`Error while checking labels: ${e}`);
+					res.sendStatus(500);
+				}
 			}
 		} else {
 			res.sendStatus(200);
@@ -98,18 +126,54 @@ async function checkLabels(pullRequest) {
 }
 
 async function reportSuccess(pullRequest, result) {
-	const url = `https://api.github.com/repos/${pullRequest.head.repo.full_name}/statuses/${pullRequest.head.sha}`;
-	let body;
+	let status;
 
 	if (result) {
-		body = {state : 'success', context : 'Consuela', description : 'Consuela says: This PR may be merged.'};
+		status = {
+			state : 'success',
+			context : 'Consuela',
+			description : 'Consuela says: This PR may be merged.'
+		};
 	} else {
-		body = {
+		status = {
 			state : 'error',
 			context : 'Consuela',
 			description : 'Consuela shouts: This PR should not be merged yet!'
 		};
 	}
+
+	await setStatus(pullRequest.head.repo.full_name, pullRequest.head.sha, status);
+}
+
+async function putMergeLock(req, res) {
+	const repo = req.query.repo;
+	const message = req.query.message ? `: ${req.query.message}` : '';
+	mergeLock = {
+		state : 'error',
+		context : 'Consuela merge-lock',
+		description : `Merge lock in place${message}`
+	};
+	const prShas = await listPullRequestsShas(repo);
+	await Promise.all(prShas.map(sha => setStatus(repo, sha, mergeLock)));
+
+	res.sendStatus(200);
+}
+
+async function releaseMergeLock(req, res) {
+	mergeLock = null;
+	const repo = req.query.repo;
+	const status = {
+		state : 'success',
+		context : 'Consuela merge-lock',
+		description : 'No merge lock!'
+	};
+	const prShas = await listPullRequestsShas(repo);
+	await Promise.all(prShas.map(sha => setStatus(repo, sha, status)));
+	res.sendStatus(200);
+}
+
+async function setStatus(repo, sha, status) {
+	const url = `https://api.github.com/repos/${repo}/statuses/${sha}`;
 
 	const response = await promisify(cb => request.post({
 		url : url,
@@ -118,12 +182,46 @@ async function reportSuccess(pullRequest, result) {
 			'User-Agent' : 'Consuela https://github.com/rogierslag/consuela',
 			'Authorization' : `token ${config.get('github.oauth_token')}`
 		},
-		body : body
+		body : status
 	}, cb));
 
 	if (!isValidResponseStatusCode(response.statusCode)) {
 		throw response;
 	}
+}
+
+async function listPullRequestsShas(repo) {
+	let shas = [];
+	let url = `https://api.github.com/repos/${repo}/pulls?per_page=10`;
+	let hasNext = true;
+	do {
+		const response = await promisify(cb => request({
+			url,
+			json : true,
+			headers : {
+				'User-Agent' : 'Consuela https://github.com/rogierslag/consuela',
+				'Authorization' : `token ${config.get('github.oauth_token')}`
+			}
+		}, cb));
+
+		shas = shas.concat(response.body.map(pr => pr.head.sha));
+
+		if (response.headers.link) {
+			const link = parseLinkHeader(response.headers.link);
+			if (link.next) {
+				url = link.next.url;
+			}
+			else {
+				hasNext = false;
+			}
+		}
+		else {
+			hasNext = false;
+		}
+	}
+	while (hasNext);
+
+	return shas;
 }
 
 function isValidResponseStatusCode(code) {
