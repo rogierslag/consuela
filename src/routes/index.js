240489a03386cb97712e850ed887804fefd3a368
Rogier Slag <rogier.slag@gmail.com>
2019-06-06 13:52:25 +0200

    Protect merge locks with a secret key

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,11 @@
 import checkHealth from './health';
 import testPayload from './test-payload';
 import labelCheck, {checkPullRequestBody} from './label-check';
-import {validateRepo, putMergeLock, releaseMergeLock} from './merge-lock';
+import {validateSecretKey, validateRepo, putMergeLock, releaseMergeLock} from './merge-lock';
 
 export default function setupRoutes(app) {
 	app.get('/health', checkHealth);
 	app.post('/', testPayload, checkPullRequestBody, labelCheck);
-	app.post('/merge-lock', validateRepo, putMergeLock);
-	app.delete('/merge-lock', validateRepo, releaseMergeLock);
+	app.post('/merge-lock', validateSecretKey, validateRepo, putMergeLock);
+	app.delete('/merge-lock', validateSecretKey, validateRepo, releaseMergeLock);
 }
